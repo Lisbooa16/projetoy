7d216d62a961d167d203cb63214ef620e96272df
Guilherme Lisboa <guilhermelisboa.b4@gmail.com>
2025-09-08 22:31:07 -0300

    pre-commit

--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -2,7 +2,12 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import GroupViewSet, UserViewSet
+from .views import (
+    GroupViewSet,
+    PasswordResetConfirmView,
+    PasswordResetRequestView,
+    UserViewSet,
+)
 
 router = DefaultRouter()
 router.register("users", UserViewSet, basename="user")
@@ -12,4 +17,14 @@ urlpatterns = [
     path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", include(router.urls)),
+    path(
+        "auth/password/reset/",
+        PasswordResetRequestView.as_view(),
+        name="password_reset",
+    ),
+    path(
+        "auth/password/reset/confirm/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
 ]
